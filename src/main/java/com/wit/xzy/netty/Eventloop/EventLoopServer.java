be5e8f002f3d79616149a325d4281ffb3a43f8e6
ZYuiX <925312844@qq.com>
2024-01-19 15:37:32 +0800

    创建多个EventLoopGroup对eventLoop进行分工细化，1.创建两个EventLoopGroup，一个用于处理accept事件，一个用于处理读写事件，对于个别事件还能独立创建EventLoopGroup并自定义handler去专门处理这个事件

--- src/main/java/com/wit/xzy/netty/Eventloop/EventLoopServer.java
+++ src/main/java/com/wit/xzy/netty/Eventloop/EventLoopServer.java
@@ -2,9 +2,7 @@ package com.wit.xzy.netty.Eventloop;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -22,9 +20,11 @@ public class EventLoopServer {
 
         //NioEventLoopGroup Worker = new NioEventLoopGroup();//默认为核心线程数*2
         //分工细化 boss worker
-        //BOSS只负责accept时间，Worker只负责socketChannel上的读写
+        //BOSS只负责accept时间，Worker只负责socketChannel上的读写时间
+
+        EventLoopGroup group = new DefaultEventLoopGroup();//创建一个额外的普通时间，可以将一些耗时较长的事件交给这个EventLoopGroup处理
         new ServerBootstrap()
-                //      boss                    Worker
+                //          bossEventLoopGroup            WorkerEventLoopGroup
                 .group(new NioEventLoopGroup(),new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(
@@ -32,6 +32,13 @@ public class EventLoopServer {
                             @Override
                             protected void initChannel(NioSocketChannel ch) throws Exception {
                                 ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                                    @Override
+                                    public void  channelRead(ChannelHandlerContext ctx, Object msg){
+                                        ByteBuf bf = (ByteBuf) msg;
+                                        log.info(Thread.currentThread().getName()+" - "+bf.toString(Charset.defaultCharset()));
+                                        ctx.fireChannelRead(msg);//这个方法可以将msg传递给下一个handler
+                                    }
+                                }).addLast(group,"handler2",new ChannelInboundHandlerAdapter(){
                                     @Override
                                     public void  channelRead(ChannelHandlerContext ctx, Object msg){
                                         ByteBuf bf = (ByteBuf) msg;
